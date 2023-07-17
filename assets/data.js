e89a5d3907279fb5903b3cd5be3dcbaefa3061a0
YousefMaher179 <ym1233445@gmail.com>
2023-07-17 06:16:58 +0300

    fix data

--- assets/data.js
+++ assets/data.js
@@ -3,7 +3,7 @@ const items = [
     id: "recmg2a1ctaEJNZhu",
     name: "utopia sofa",
     image: {
-      url: "../../assets/images/img1.jpg",
+      url: "https://i.pinimg.com/564x/ab/84/93/ab849324fa828dbf6f1f7b7d28b7a06e.jpg",
     },
     price: 39.95,
   },
@@ -11,7 +11,7 @@ const items = [
     id: "recvKMNR3YFw0bEt3",
     name: "entertainment center",
     image: {
-      url: "../../assets/images/img2.jpg",
+      url: "https://i.pinimg.com/564x/a4/95/39/a4953940968dced1c0765e67a7b86527.jpg",
     },
     price: 29.98,
   },
@@ -19,7 +19,7 @@ const items = [
     id: "recxaXFy5IW539sgM",
     name: "albany sectional",
     image: {
-      url: "../../assets/images/img3.jpg",
+      url: "https://i.pinimg.com/564x/29/df/fb/29dffb182f3b23322df51b9a5ba0bc78.jpg",
     },
     price: 10.99,
   },
@@ -27,7 +27,7 @@ const items = [
     id: "recyqtRglGNGtO4Q5",
     name: "leather sofa",
     image: {
-      url: "../../assets/images/img4.png",
+      url: "https://i.pinimg.com/564x/6a/73/96/6a7396c73efba7ce2f10d538d8d7bf67.jpg",
     },
     price: 9.99,
   },
